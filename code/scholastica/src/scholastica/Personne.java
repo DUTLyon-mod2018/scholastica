5c6bf325906a430435c9f9493a5c5ca241192e19
OUCHANE NABIL p0204580 <p0204580@univ-lyon1.fr>
2018-03-26 19:31:18 +0200

    Nettoyage des fichiers classes Personnes et enseignants

--- code/scholastica/src/scholastica/Personne.java
+++ code/scholastica/src/scholastica/Personne.java
@@ -1,47 +1,25 @@
-/***********************************************************************
- * Module:  Personne.java
- * Author:  x7000328
- * Purpose: Defines the Class Personne
- ***********************************************************************/
 
 import java.util.*;
 
-/** @pdOid 9bac2351-e8e3-4f9f-8fd7-4275909f8177 */
 public class Personne {
-   /** @pdOid eb453c4e-cfc0-4dc9-a888-dcc9c0c63174 */
+ 
    protected String nom;
-   /** @pdOid 96dcb44c-45b4-49c8-aeec-124f8b15fda3 */
    protected String prenom;
-   
-   /** @param Nom 
-    * @param Prenom
-    * @pdOid 71a1ee89-f2c4-4a4f-b698-b6946da641d8 */
-   public void personne(String Nom, String Prenom) {
-      // TODO: implement
-   }
-   
-   /** @pdOid ac694c3c-0b31-4eff-9af6-34f3f35b7caa */
+   public void personne(String _nom, String _prenom) {
+       nom=_nom;
+       prenom=_prenom;
+       
+    }
    public String getNom() {
-      // TODO: implement
-      return null;
+       return nom;
    }
-   
-   /** @pdOid ace553d4-4e87-48f9-a700-6fdb0a97fa14 */
-   public String getPrenom() {
-      // TODO: implement
-      return null;
+      public String getPrenom() {
+      return prenom;
    }
-   
-   /** @param Nom
-    * @pdOid 3ff5f6f7-5b48-43ca-9ab7-34e2311c32a4 */
-   public void setNom(String Nom) {
-      // TODO: implement
+     public void setNom(String _nom) {
+         nom=_nom;
    }
-   
-   /** @param Prenom
-    * @pdOid d3bd13e3-412a-457c-bdab-e38b9c725f61 */
-   public void setPrenom(String Prenom) {
-      // TODO: implement
+   public void setPrenom(String _prenom) {
+         prenom=_prenom;
    }
-
 }
